008cd75dac9b4bd89e21e35639520a9a2b9afc4b
Damacy64 <pzx6464@gmail.com>
2021-12-18 13:58:46 -0600

    se ingresan todas las opciones dentro del case

--- comandos.sh
+++ comandos.sh
@@ -7,16 +7,16 @@ while [ $opcion -ne 61 ]; do
 	echo "1) comando pwd"
 	echo "2) comando cd"
 	echo "3) comando ls"
-	echo "4) comando cat comandos.sh"
+	echo "4) comando cat archivo.txt"
 	echo "5) comando cp archivo.txt /home/damacy/Proyecto/carpeta"
 	echo "6) comando mv"
 	echo "7) comando mkdir prueba"
 	echo "8) comando rmdir prueba"
-	echo "9) comando rm"
-	echo "10) comando touch"
+	echo "9) comando rm archivo.txt"
+	echo "10) comando touch archivo.txt"
 	echo "11) comando sudo" 
-	echo "12) comando head misdatos.sh"
-	echo "13) comando tail misdatos.sh"
+	echo "12) comando head archivo.txt"
+	echo "13) comando tail archivo.txt"
 	echo "14) comando chmod"
 	echo "15) comando jobs"
 	echo "16) comando ping"
@@ -76,6 +76,8 @@ while [ $opcion -ne 61 ]; do
 			echo este comando nos muestra la ruta que sigue para llegar a nuestra posicion actual
 			echo presione enter para ejecutar el comando
 			read $enter
+			echo el directorio actual es:
+			echo
 			pwd 
 			read $enter
 			clear;;
@@ -84,7 +86,15 @@ while [ $opcion -ne 61 ]; do
 			echo este comando nos cambia de directorio, recibe como argumento el directorio al que queremos ir
 			echo presione enter para ejecutar el comando
 			read $enter
+			echo el directorio actual es:
+			echo
+			pwd
 			cd carpeta/
+			echo
+			echo una vez ejecutado el comando la ubicacion actual es:
+			echo
+			pwd
+			cd ..
 			read $enter
 			clear;;
 		3)
@@ -100,7 +110,7 @@ while [ $opcion -ne 61 ]; do
 			echo este comando nos muestra lo que contiene cierto archivo, recibe como argumento el archivo
 			echo presione enter para ejecutar el comando
 			read $enter
-			cat comandos.sh
+			cat archivo.txt
 			read $enter
 			clear;;
 		5)
@@ -110,6 +120,8 @@ while [ $opcion -ne 61 ]; do
 			read $enter
 			cp archivo.txt /home/damacy/Proyecto/carpeta
 			cd carpeta/
+			echo el archivo copiado fue archivo.txt y se copio con exito en el directorio carpeta
+			echo
 			ls
 			rm archivo.txt
 			read $enter
@@ -119,11 +131,17 @@ while [ $opcion -ne 61 ]; do
 			echo este comando mueve un archivo a otro directorio
 			echo presione enter para ejecutar el comando
 			read $enter
+			echo archivo.txt dentro de la carpeta actual:
+			echo
 			ls
 			mv archivo.txt carpeta/
 			cd carpeta/
+			echo nos movemos a la carpeta que se envio el archivo, y ejecutamos ls:
+			echo
 			ls
 			cd ../
+			echo regresamos a la carpeta base, y corroboramos que el archivo ya no esta en su antiguo lugar
+			echo
 			ls
 			cd carpeta/
 			mv archivo.txt ../
@@ -134,8 +152,13 @@ while [ $opcion -ne 61 ]; do
 			echo este comando crea un directorio, recibe como argumento el nombre del directorio
 			echo presione enter para ejecutar el comando
 			read $enter
+			echo aun no existe el directorio prueba:
+			echo
 			ls
 			mkdir prueba
+			echo
+			echo directorio prueba creado con existo:
+			echo
 			ls
 			rmdir prueba
 			read $enter
@@ -146,18 +169,219 @@ while [ $opcion -ne 61 ]; do
 			echo presione enter para ejecutar el comando
 			read $enter
 			mkdir prueba
+			echo carpeta prueba existente:
+			echo
 			ls
 			rmdir prueba
+			echo
+			echo carpeta prueba eliminada con exito:
+			echo
 			ls
 			read $enter
 			clear;;
 		9)
+			clear
+			echo este comando elimina un fichero, recibe como argumento el fichero que se eliminar
+			echo presione enter para ejecutar el comando
+			read $enter
+			echo fichero existente:
+			echo
+			ls
+			rm archivo.txt
+			echo
+			echo fichero eliminado con existo:
+			echo
+			ls
+			touch archivo.txt
+			read $enter
+			clear;;
+		10)
+			clear
+			echo este comando crea un archivo, recibe como parametro el nombre del archivo
+			echo presione enter para ejecutar el comando
+			read $enter
+			rm archivo.txt
+			echo directorio sin el archivo:
+			echo
+			ls
+			echo
+			touch archivo.txt
+			echo creacion del archivo:
+			echo
+			ls
+			read $enter
+			clear;;
+		11)
+			clear
+			echo este comando nos permite ejecutar comandos como otro usuario debe de ir acompañado de otro comando
+			echo lo siguiente es la ayuda del comando, enter para ejecutar el comando
+			read $enter
+			sudo -h
+			read $enter
+			clear;;
+		12)
+			clear
+			echo este comando nos muestra las primeras 10 lineas de un archivo de texto
+			echo enter para ejecutar el comando
+			read $enter
+			head archivo.txt
+			read $enter
+			clear;;
+		13)
+			clear
+			echo este comando nos muestra las ultimas 10 lineas de un archivo de texto
+			echo enter para ejecutar el comando
+			read $enter
+			tail archivo.txt
+			read $enter
+			clear;;
+		14)
+			clear
+			echo este comando nos permite cambiar las propiedades que tienen los archivos, debe usarse con precaucion
+			echo enter para ejecutar el comando
+			read $enter
+			echo podemos ver que el fichero [archivo.txt] tiene propiedades de lectura y escritura
+			ls -l
+			echo
+			echo ahora solo le daremos permiso de lectura con el comando chmod
+			echo
+			chmod 444 archivo.txt
+			ls -l
+			chmod 664 archivo.txt
+			read $enter
+			clear;;
+		15)
+			clear
+			echo este comando nos muestra los trabajos que se estan ejecutando en la computadora
+			echo enter para ejecutar el comando
+			read $enter
+			echo en caso de no haber trabajos, no mostrara nada
+			jobs
+			read $enter
+			clear;;
+		16)
+			clear
+			echo "este comando envía un pequeño paquete de datos al sistema de destino indicado (dominio o IP)
+y evalúa el tiempo que transcurre hasta que se registra una respuesta."
+			echo enter para ejecutar el comando
+			read $enter
+			ping localhost -c 5
+			read $enter
+			clear;;
+		17)
+			clear
+			echo este comando nos imprima la informacion del kernel
+			echo enter para ejecutar el comando
+			read $enter
+			uname
+			read $enter
+			clear;;
+		18)
+			clear
+			echo este comando mostrará una lista de los procesos en ejecución y la cantidad de CPU que utiliza cada proceso.
+			echo para salir de top es necesario presionar ctrl + c
+			echo enter para ejecutar el comando
+			read $enter
+			top
+			clear;;
+		19)
+			clear
+			echo este comando nos muestra un historial de todos los comando que hemos ocupado en el shell
+			echo enter para ejecutar el comando
+			read $enter
+			history
+			read $enter
+			clear;;
+		20)
+			clear
+			echo este comando nos abre el manual del comando que pasemos como argumento
+			echo enter para ejecutar el comando
+			read $enter
+			man man
+			clear;;
+		21)
+			;;
+		22)
+			;;
+		23)
+			;;
+		24)
+			;;
+		25)
+			;;
+		26)
+			;;
+		27)
+			;;
+		28)
+			;;
+		29)
+			;;
+		30)
+			;;
+		31)
+			;;
+		32)
+			;;
+		33)
+			;;
+		34)
+			;;
+		35)
+			;;
+		36)
+			;;
+		37)
+			;;
+		38)
+			;;
+		39)
+			;;
+		40)
+			;;
+		41)
+			;;
+		42)
+			;;
+		43)
+			;;
+		44)
+			;;
+		45)
+			;;
+		47)
+			;;
+		48)
+			;;
+		49)
+			;;
+		50)
+			;;
+		51)
+			;;
+		52)
+			;;
+		53)
+			;;
+		54)
+			;;
+		55)
+			;;
+		56)
+			;;
+		57)
+			;;
+		58)
+			;;
+		59)
+			;;
+		60)
 			;;
 		61)
 			clear
 			opcion=61;;
 		*)
-			echo opcion no valida
+			echo *****************opcion no valida*****************
 			opcion=0;;
 	esac
 
