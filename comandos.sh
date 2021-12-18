64f7602ca0711023011f8305415245a2796519ce
Damacy64 <pzx6464@gmail.com>
2021-12-17 23:16:53 -0600

    se agrega mas funcionalidades

--- comandos.sh
+++ comandos.sh
@@ -3,15 +3,15 @@
 clear
 opcion=0
 while [ $opcion -ne 61 ]; do
-	echo inserte un numero, para ejecutar el comando indicado, en caso de querer salir seleccione el numero 61
+	echo -------inserte un numero, para ejecutar el comando indicado, [en caso de querer salir seleccione el numero 61]-------
 	echo "1) comando pwd"
 	echo "2) comando cd"
 	echo "3) comando ls"
-	echo "4) comando cat"
-	echo "5) comando cp"
+	echo "4) comando cat comandos.sh"
+	echo "5) comando cp archivo.txt /home/damacy/Proyecto/carpeta"
 	echo "6) comando mv"
-	echo "7) comando mkdir"
-	echo "8) comando rmdir"
+	echo "7) comando mkdir prueba"
+	echo "8) comando rmdir prueba"
 	echo "9) comando rm"
 	echo "10) comando touch"
 	echo "11) comando sudo" 
@@ -32,7 +32,8 @@ while [ $opcion -ne 61 ]; do
 	echo "26) comando tree"
 	echo "27) comando who"
 	echo "28) comando date"
-	sleep 2
+	echo -------------------------enter para ver mas comandos---------------------
+	read $enter
 	echo "29) comando time"
 	echo "30) comando uptime"
 	echo "31) comando nano"
@@ -66,7 +67,7 @@ while [ $opcion -ne 61 ]; do
 	echo "59) comando grep ls Proyecto/comandos.sh"
 	echo "60) comando uname"
 	echo "61) salir del programa"
-	echo seleccione una opcion
+	echo -----------------seleccione una opcion--------------------
 	read opcion
 	
 	case $opcion in
@@ -74,16 +75,84 @@ while [ $opcion -ne 61 ]; do
 			clear
 			echo este comando nos muestra la ruta que sigue para llegar a nuestra posicion actual
 			echo presione enter para ejecutar el comando
-			read
+			read $enter
 			pwd 
-			sleep 5;;
+			read $enter
+			clear;;
 		2)
 			clear
 			echo este comando nos cambia de directorio, recibe como argumento el directorio al que queremos ir
 			echo presione enter para ejecutar el comando
-			read
-			cd Proyecto/
-			sleep 5;;
+			read $enter
+			cd carpeta/
+			read $enter
+			clear;;
+		3)
+			clear
+			echo este comando nos muestra los archivos y directorios que hay en el directorio actual
+			echo presione enter para ejecutar el comando
+			read $enter
+			ls
+			read $enter
+			clear;;
+		4)
+			clear
+			echo este comando nos muestra lo que contiene cierto archivo, recibe como argumento el archivo
+			echo presione enter para ejecutar el comando
+			read $enter
+			cat comandos.sh
+			read $enter
+			clear;;
+		5)
+			clear
+			echo este comando nos copia un archivo y lo pega en un directorio nuevo
+			echo presione enter para ejecutar el comando
+			read $enter
+			cp archivo.txt /home/damacy/Proyecto/carpeta
+			cd carpeta/
+			ls
+			rm archivo.txt
+			read $enter
+			clear;;
+		6)
+			clear
+			echo este comando mueve un archivo a otro directorio
+			echo presione enter para ejecutar el comando
+			read $enter
+			ls
+			mv archivo.txt carpeta/
+			cd carpeta/
+			ls
+			cd ../
+			ls
+			cd carpeta/
+			mv archivo.txt ../
+			read $enter
+			clear;;
+		7)
+			clear
+			echo este comando crea un directorio, recibe como argumento el nombre del directorio
+			echo presione enter para ejecutar el comando
+			read $enter
+			ls
+			mkdir prueba
+			ls
+			rmdir prueba
+			read $enter
+			clear;;
+		8)
+			clear
+			echo este comando elimina un directorio, recibe como argumento el directorio a eliminar
+			echo presione enter para ejecutar el comando
+			read $enter
+			mkdir prueba
+			ls
+			rmdir prueba
+			ls
+			read $enter
+			clear;;
+		9)
+			;;
 		61)
 			clear
 			opcion=61;;
